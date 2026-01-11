7ebc71b0c119ef1d38b7147211a12748acd10a59
barneserella <girlthisismycodingemail@gmail.com>
2026-01-11 16:25:28 -0600

    part 5-C notesApp-testing react apps

---
+++ notesApp/src/components/Note.test.jsx
@@ -0,0 +1,51 @@
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import Note from './Note'
+
+test('renders content', () => {
+  const note = {
+    content: 'Component testing is done with react-testing-library',
+    important: true
+  }
+
+  render(<Note note={note} />)
+
+
+
+  const element = screen.getByText('Component testing is done with react-testing-library')
+
+  expect(element).toBeDefined()
+})
+
+test('renders content', () => {
+  const note = {
+    content: 'Does not work anymore :(',
+    important: true
+  }
+
+  render(<Note note={note} />)
+
+  const element = screen.getByText('Does not work anymore :(', { exact: false } )
+
+  screen.debug(element)
+
+  expect(element).toBeDefined()
+})
+test('clicking the button calls event handler once', async () => {
+  const note = {
+    content: 'Component testing is done with react-testing-library',
+    important: true
+  }
+  
+  const mockHandler = vi.fn()
+
+  render(
+    <Note note={note} toggleImportance={mockHandler} />
+  )
+
+  const user = userEvent.setup()
+  const button = screen.getByText('make not important')
+  await user.click(button)
+
+  expect(mockHandler.mock.calls).toHaveLength(1)
+})
